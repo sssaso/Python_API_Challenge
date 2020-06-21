138a82e055cd40d5da71bdbe5e6d7b6d80136dda
sssaso <63740719+sssaso@users.noreply.github.com>
2020-06-21 16:15:03 -0400

    removd my gkey
    removed my api key

--- Part_II_VacationPy/api_keys.py
+++ Part_II_VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "51b0a4c17feb28f07bf3b8b8bcbb5501"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyCV-iQQ1eQywGEe_XBJcL4ucdRiulUpyhw"
+g_key = ""
